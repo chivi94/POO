c51a4653e9f07250f78c3d60b250f358da25ed89
Iván González Rincón <ivan.gonzalez.rincon@alumnos.uva.es>
2016-09-21 12:59:39 +0200

    Add files via upload

---
+++ Racional/src/ejemplos/Racional.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplos;
+
+/**
+ *
+ * @author Ivan
+ * Clase que implementa un numero Racional
+ */
+public class Racional {
+
+    private int numerador;
+    private int denominador;
+
+    public Racional() {
+
+    }
+
+    public Racional(int numerador, int denominador) {
+        this.numerador = numerador;
+        this.denominador = denominador;
+    }
+
+    public Racional suma(Racional r) {
+        int n = (this.numerador * r.getDenominador()) + (this.denominador * r.getNumerador());
+        int d = this.denominador * r.getDenominador();
+
+        return new Racional(n, d);
+    }
+
+    public Racional opuesto() {
+        return new Racional(-this.numerador, this.denominador);
+    }
+
+    public Racional resta() {
+        return suma(opuesto());
+    }
+
+    public Racional multiplica(Racional r) {
+        return new Racional(this.numerador + r.getNumerador(), this.denominador + r.getDenominador());
+    }
+
+    public Racional inverso() {
+        return new Racional(this.denominador, this.numerador);
+    }
+
+    public Racional divide(Racional r) {
+        return multiplica(r.inverso());
+    }
+
+    public int getNumerador() {
+        return numerador;
+    }
+
+    public int getDenominador() {
+        return denominador;
+    }
+
+    @Override
+    public String toString() {
+        return this.numerador + "/" + this.denominador;
+    }
+
+}
